11b7e698e5c973569cfad8082f2e954fa6a2a443
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-04-23 23:04:48 +0200

    Initial Commit

---
+++ Boot_Validation/Boot_ValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boot_ValidationApp.swift
+//  Boot_Validation
+//
+//  Created by Miguel Gallego on 23/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Boot_ValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
