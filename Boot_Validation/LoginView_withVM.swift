43ac851e1f0499af9df28163ad8c1f7ddfa44d12
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-05-02 21:37:29 +0200

    LoginView_withVM done

--- Boot_Validation/LoginView_withVM.swift
+++ Boot_Validation/LoginView_withVM.swift
@@ -4,29 +4,63 @@
 import SwiftUI
 
 struct LoginView_withVM: View {
-    @State private var email: String = ""
-    @State private var password: String = ""
+    @State private var loginState = LoginState()
     
     var body: some View {
         Form {
-            TextField("Email", text: $email)
-                .textInputAutocapitalization(.never)
-            SecureField("Password", text: $password)
+            VStack(alignment: .leading) {
+                TextField("Email", text: $loginState.email)
+                    .textInputAutocapitalization(.never)
+                if let emailError = loginState.emailError {
+                    Text(emailError.localizedDescription)
+                        .font(.caption).foregroundStyle(.red)
+                }
+            }
+            VStack(alignment: .leading)  {
+                SecureField("Password", text: $loginState.password)
+                if let passwordError = loginState.passwordError {
+                    Text(passwordError.localizedDescription)
+                        .font(.caption).foregroundStyle(.red)
+                }
+            }
             Button("Login") {
-                
+                if loginState.isValid() {
+                    // ...
+                }
             }
         }
     }
     
-    // MARK: - Logic
-    private var isFormValid: Bool {
-        !password.isEmpty && email.isValidEmail
-    }
-
 }
 
-struct LoginState {
+struct LoginState {             // You can write this in another file
+    var email: String = ""
+    var password: String = ""
+    var emailError: LoginError?
+    var passwordError: LoginError?
     
+    mutating func isValid() -> Bool {
+        clearErrors()
+        
+        if email.isEmpty {
+            emailError = .emailEmpty
+            return false
+        } else if !email.isValidEmail {
+            emailError = .emailInvalid
+            return false
+        }
+        
+        if password.isEmpty {
+            passwordError = .passwordEmpty
+            return false
+        }
+        return true
+    }
+    
+    mutating func clearErrors() {
+        emailError = nil
+        passwordError = nil
+    }
 }
 
 
@@ -34,6 +68,8 @@ enum LoginError: Error, LocalizedError {
     case emailEmpty
     case emailInvalid
     case passwordEmpty
+    
+    // var recoverySuggestion: String? { } // Implementación opcional
     
     var errorDescription: String? {
         switch self {
